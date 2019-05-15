f3e44c0a7fd3283a332b0d769e18347b844de428
mingo <domingo.em@gmail.com>
2019-05-15 10:56:05 +0200

    Refactor to streams

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,18 +1,17 @@
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class StringCalculator {
 
     public int add(String numbers) {
-        if (numbers.equals("")) {
+        if (numbers.isEmpty()) {
             return 0;
         }
         return sumNumbers(numbers);
     }
 
     private int sumNumbers(String numbers) {
-        String[] numbersArray = numbers.split(",|\n");
-        int result = 0;
-        for (String number: numbersArray){
-            result += Integer.valueOf(number);
-        }
-        return result;
+        Stream<String> numbersArray = Arrays.stream(numbers.split(",|\n"));
+        return numbersArray.mapToInt(Integer::parseInt).sum();
     }
 }
