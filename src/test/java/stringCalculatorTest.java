d1b8e6593216638ac8343874d53de7e4c816280f
mingo <domingo.em@gmail.com>
2019-05-15 10:58:40 +0200

    Refactor imports

--- src/test/java/stringCalculatorTest.java
+++ src/test/java/stringCalculatorTest.java
@@ -1,6 +1,7 @@
-import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class stringCalculatorTest {
@@ -8,27 +9,27 @@ public class stringCalculatorTest {
     public void simple_calculator() {
         StringCalculator stringCalculator = new StringCalculator();
         int result = stringCalculator.add("");
-        MatcherAssert.assertThat(result, Matchers.is(0));
+        assertThat(result, Matchers.is(0));
     }
 
     @Test
     public void simple_calculator_various_parameters() {
         StringCalculator stringCalculator = new StringCalculator();
         int resultAdd = stringCalculator.add("4,2");
-        MatcherAssert.assertThat(resultAdd, Matchers.is(6));
+        assertThat(resultAdd, Matchers.is(6));
     }
     
     @Test
     public void given_arbitrary_number_size_then_return_the_sum() {
         StringCalculator stringCalculator = new StringCalculator();
         int resultAdd = stringCalculator.add("1,2,3,4,5,6,7,8,9");
-        MatcherAssert.assertThat(resultAdd, Matchers.is(45));
+        assertThat(resultAdd, Matchers.is(45));
     }
     
     @Test
     public void given_new_line_separator_then_return_the_sum() {
         StringCalculator stringCalculator = new StringCalculator();
         int resultAdd = stringCalculator.add("1\n2,3");
-        MatcherAssert.assertThat(resultAdd, Matchers.is(6));
+        assertThat(resultAdd, Matchers.is(6));
     }
 }
